bf2388d82905ef32ebd53f87f17d8d5360416592
Bron Gondwana <brong@fastmail.fm>
2015-07-04 07:44:04 -0500

    ICloud: just wrap standard with some magic ports

--- JMAP/Sync/ICloud.pm
+++ JMAP/Sync/ICloud.pm
@@ -4,113 +4,19 @@ use strict;
 use warnings;
 
 package JMAP::Sync::ICloud;
-use base qw(JMAP::Sync::Common);
+use base qw(JMAP::Sync::Standard);
 
-use Mail::IMAPTalk;
-use JSON::XS qw(encode_json decode_json);
-use Email::Simple;
-use Email::Sender::Simple qw(sendmail);
-use Email::Sender::Transport::SMTPS;
-use Net::CalDAVTalk;
-use Net::CardDAVTalk;
-
-my %KNOWN_SPECIALS = map { lc $_ => 1 } qw(\\HasChildren \\HasNoChildren \\NoSelect \\NoInferiors);
-
-sub connect_calendars {
-  my $Self = shift;
-
-  if ($Self->{calendars}) {
-    $Self->{lastused} = time();
-    return $Self->{calendars};
-  }
-
-  $Self->{calendars} = Net::CalDAVTalk->new(
-    user => $Self->{auth}{username},
-    password => $Self->{auth}{password},
-    url => "https://caldav.icloud.com/",
-    expandurl => 1,
+sub new {
+  my $class = shift;
+  my $auth = shift;
+  my %a = (
+    imapserver => 'imap.mail.me.com',
+    smtpserver => 'smtp.mail.me.com',
+    calurl => 'https://caldav.icloud.com/',
+    aburl => 'https://contacts.icloud.com/',
+    %$auth,
   );
-
-  return $Self->{calendars};
-}
-
-sub connect_contacts {
-  my $Self = shift;
-
-  if ($Self->{contacts}) {
-    $Self->{lastused} = time();
-    return $Self->{contacts};
-  }
-
-  $Self->{contacts} = Net::CardDAVTalk->new(
-    user => $Self->{auth}{username},
-    password => $Self->{auth}{password},
-    url => "https://contacts.icloud.com/",
-    expandurl => 1,
-  );
-
-  return $Self->{contacts};
-}
-
-sub connect_imap {
-  my $Self = shift;
-
-  if ($Self->{imap}) {
-    $Self->{lastused} = time();
-    return $Self->{imap};
-  }
-
-  for (1..3) {
-    $Self->log('debug', "Looking for server for $Self->{auth}{username}");
-    my $port = 993;
-    my $usessl = $port != 143;  # we use SSL for anything except default
-    $Self->log('debug', "getting imaptalk");
-    $Self->{imap} = Mail::IMAPTalk->new(
-      Server   => 'imap.mail.me.com',
-      Port     => $port,
-      Username => $Self->{auth}{username},
-      Password => $Self->{auth}{password},
-      # not configurable right now...
-      UseSSL   => $usessl,
-      UseBlocking => $usessl,
-    );
-    next unless $Self->{imap};
-    $Self->log('debug', "Connected as $Self->{auth}{username}");
-    $Self->{lastused} = time();
-    my $list = $Self->{imap}->capability()->{xlist} ? 'xlist' : 'list';
-    my @folders = $Self->{imap}->$list('', '*');
-
-    delete $Self->{folders};
-    delete $Self->{labels};
-    foreach my $folder (@folders) {
-      my ($role) = grep { not $KNOWN_SPECIALS{lc $_} } @{$folder->[0]};
-      my $name = $folder->[2];
-      my $label = $role || $folder->[2];
-      $Self->{folders}{$name} = $label;
-      $Self->{labels}{$label} = $name;
-    }
-    return $Self->{imap};
-  }
-
-  die "Could not connect to IMAP server: $@";
-}
-
-sub send_email {
-  my $Self = shift;
-  my $rfc822 = shift;
-
-  my $email = Email::Simple->new($rfc822);
-  sendmail($email, {
-    from => $Self->{auth}{username},
-    transport => Email::Sender::Transport::SMTPS->new({
-      helo => 'proxy.jmap.io',
-      host => 'smtp.mail.me.com',
-      port => 587,
-      ssl => 'starttls',
-      sasl_username => $Self->{auth}{username},
-      sasl_password => $Self->{auth}{password},
-    }),
-  });
+  return JMAP::Sync::Standard->new(\%a);
 }
 
 1;
