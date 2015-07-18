5198a314244c685886c1a693ec96c0cc8f75c416
Bron Gondwana <brong@fastmail.fm>
2015-07-18 03:11:27 -0400

    carduid

--- JMAP/DB.pm
+++ JMAP/DB.pm
@@ -728,10 +728,10 @@ sub delete_card {
   my $carduid = shift;
   my $kind = shift;
   if ($kind eq 'contact') {
-    $Self->dupdate('jcontacts', {active => 0}, {carduid => $carduid, jaddressbookid => $jaddressbookid});
+    $Self->dupdate('jcontacts', {active => 0}, {contactuid => $carduid, jaddressbookid => $jaddressbookid});
   }
   else {
-    $Self->dupdate('jcontactgroupmap', {active => 0}, {carduid => $carduid, jaddressbookid => $jaddressbookid});
+    $Self->dupdate('jcontactgroupmap', {active => 0}, {groupuid => $carduid, jaddressbookid => $jaddressbookid});
   }
 }
 
