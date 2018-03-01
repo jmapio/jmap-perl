d3709f81979306f89c4667d3f7a87ab380752772
Bron Gondwana <brong@fastmail.fm>
2018-02-28 20:27:25 -0500

    API: dummy StorageNode API

--- JMAP/API.pm
+++ JMAP/API.pm
@@ -3163,4 +3163,93 @@ sub api_EmailSubmission_set {
   return @res;
 }
 
+sub api_StorageNode_query {
+  my $Self = shift;
+  my $args = shift;
+
+  $Self->begin();
+  my $dbh = $Self->{db}->dbh();
+
+  my $user = $Self->{db}->get_user();
+  my $accountid = $Self->{db}->accountid();
+  return $Self->_transError(['error', {type => 'accountNotFound'}])
+    if ($args->{accountId} and $args->{accountId} ne $accountid);
+
+  my $newState = 'dummy';
+
+  my $start = $args->{position} || 0;
+  return $Self->_transError(['error', {type => 'invalidArguments'}])
+    if $start < 0;
+
+  my $data = [];
+
+  my $end = $args->{limit} ? $start + $args->{limit} - 1 : $#$data;
+  $end = $#$data if $end > $#$data;
+
+  my @result = map { $data->[$_][0] } $start..$end;
+
+  $Self->commit();
+
+  return ['StorageNode/query', {
+    accountId => $accountid,
+    filter => $args->{filter},
+    state => $newState,
+    position => $start,
+    total => scalar(@$data),
+    ids => [map { "$_" } @result],
+  }];
+}
+
+sub api_StorageNode_get {
+  my $Self = shift;
+  my $args = shift;
+
+  $Self->begin();
+  my $dbh = $Self->{db}->dbh();
+
+  my $user = $Self->{db}->get_user();
+  my $accountid = $Self->{db}->accountid();
+  return $Self->_transError(['error', {type => 'accountNotFound'}])
+    if ($args->{accountId} and $args->{accountId} ne $accountid);
+
+  my $newState = 'dummy';
+
+  #properties: String[] A list of properties to fetch for each message.
+
+  my $data = {};
+
+  my %want;
+  if ($args->{ids}) {
+    %want = map { $Self->idmap($_) => 1 } @{$args->{ids}};
+  }
+  else {
+    %want = %$data;
+  }
+
+  my @list;
+  foreach my $id (keys %want) {
+    next unless $data->{$id};
+    delete $want{$id};
+
+    my $item = decode_json($data->{$id}{payload});
+
+    foreach my $key (keys %$item) {
+      delete $item->{$key} unless _prop_wanted($args, $key);
+    }
+
+    $item->{id} = $id;
+
+    push @list, $item;
+  }
+  $Self->commit();
+
+  return ['StorageNode/get', {
+    list => \@list,
+    accountId => $accountid,
+    state => $newState,
+    notFound => (%want ? [keys %want] : undef),
+  }];
+}
+
+
 1;
